d539be33e27d9658d3063026a57fe556a20d1888
Lukas Wagner <lukas.wagner@hpi.de>
2020-03-03 15:59:05 +0100

    removed outdated csv docs for now

--- source/code/frontend/csv.ts
+++ source/code/frontend/csv.ts
@@ -1,16 +1,3 @@
-
-/**
- * Given a CSV with comma separated (',') values and a head line containing for 'x', 'y', and 'z' identifiers, this
- * identifies the column indices of x, y, and z, ignores all empty lines, and parses floats for every component
- * respectively. Since a file list is expected and processing takes some time, a Promis on an Array of Float32Arrays
- * is created and returned when all CSV files where parsed/imported.
- * ```
- * const input = document.getElementById('input-file')! as HTMLInputElement;
- * input.addEventListener('change', () => {
- *     importPointsFromCSV(input.files!).then(result => console.log(result));
- * });
- * ```
- */
 export function importPointsFromCSV(list: Array<string>, progress?: HTMLProgressElement): Promise<Array<Float32Array>> {
     if (progress) {
         progress.max = list.length;
