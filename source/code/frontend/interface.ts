ff5489b7fafa2908a221f58ae5991b66231afaa0
Lukas Wagner <lukas@lwgnr.dev>
2022-02-16 16:31:32 +0100

    add declaration for external interface

---
+++ source/code/frontend/interface.ts
@@ -0,0 +1,30 @@
+import { Column, DataSource } from '@lukaswagner/csv-parser';
+
+export type ReadyMessage = {
+    type: 'ready'
+}
+
+export interface Configuration {
+    name?: string;
+    csv?: DataSource;
+    delimiter?: string;
+    pointSize?: number;
+    axes?: string[];
+    colorMode?: number;
+    colorMapping?: number;
+    colorColumn?: string;
+    variablePointSizeStrength?: number;
+    variablePointSizeColumn?: string;
+}
+
+export type ConfigurationMessage = {
+    type: 'configuration',
+    data: Configuration
+}
+
+export type ColumnsMessage = {
+    type: 'columns',
+    data: Column[]
+}
+
+export type Message = ReadyMessage | ConfigurationMessage | ColumnsMessage;
